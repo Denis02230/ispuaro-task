0980d137a040a1216a54b4c9920650020edad028
Denis02230 <s02230011@gse.cs.msu.ru>
2025-03-23 14:31:01 +0300

    adjust run.sh to new paths

--- source/run.sh
+++ source/run.sh
@@ -31,7 +31,7 @@ echo "generating a base SBOM with syft"
 syft gcc -o cyclonedx-json > .no_cves-gcc-sbom.cdx.json
 
 echo "running add_components.py to add more components to SBOM"
-python3 add_components.py -i .no_cves-gcc-sbom.cdx.json -s gcc/
+python3 -m mysbomtools.add_components -i .no_cves-gcc-sbom.cdx.json -s gcc/
 
 echo "generating a vulnerabilities JSON with cve-bin-tool"
 cve-bin-tool -f json -o .cves.json gcc
@@ -40,7 +40,7 @@ CVEBINTOOL_VERSION=$(cve-bin-tool --version | head -n 1 | awk '{print $1}')
 GCC_VERSION=$(gcc --version | head -n 1 | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
 
 echo "running merge.py to add vulnerabilities to SBOM"
-python3 merge.py .no_cves-gcc-sbom.cdx.json .cves.json gcc-sbom.cdx.json $CVEBINTOOL_VERSION $GCC_VERSION
+python3 -m mysbomtools.merge .no_cves-gcc-sbom.cdx.json .cves.json gcc-sbom.cdx.json $CVEBINTOOL_VERSION $GCC_VERSION
 
 echo "cleaning up temporary files"
 rm .no_cves-gcc-sbom.cdx.json

8b759fa8664aa48d139e792ed60fe03714a1f067
Титов Денис <s02230011@gse.cs.msu.ru>
2025-03-23 11:26:14 +0300

    consistent file naming with part 1 of the task

--- binaries/run.sh
+++ binaries/run.sh
@@ -1,8 +1,8 @@
 mkdir -p gcc_binaries
 tar -xf gcc_binaries.tar.gz -C gcc_binaries
 
-syft gcc_binaries/ -o cyclonedx-json > .no_cves-gcc-bin-sbom.json
+syft gcc_binaries/ -o cyclonedx-json > .no_cves-gcc-bin-sbom.cdx.json
 
-python3 add_vulns.py .no_cves-gcc-bin-sbom.json gcc-bin-sbom.json
+python3 add_vulns.py .no_cves-gcc-bin-sbom.cdx.json gcc-bin-sbom.cdx.json
 
-rm .no_cves-gcc-bin-sbom.json
+rm .no_cves-gcc-bin-sbom.cdx.json
